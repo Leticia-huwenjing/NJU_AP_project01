bb5cc3c9ade8ae0a585d1ce2358187cc44f7631c
Leticia-huwenjing <1502503185@qq.com>
2022-02-19 23:50:32 +0800

    The corresponding files are initialized 3.3

--- data.h
+++ data.h
@@ -5,4 +5,29 @@
 #ifndef PROJECT01__DATA_H_
 #define PROJECT01__DATA_H_
 
+#include <iostream>
+using namespace std;
+
+string user_title = "用户ID,用户名,密码,联系方式,地址,钱包余额";
+string good_title = "商品ID,名称,价格,数量,描述,卖家ID,上架时间,商品状态";
+string order_title = "订单ID,商品ID,交易单价,数量,交易时间,卖家ID,买家ID";
+string command_file = "command.txt";
+string good_file = "commodity.txt";
+string order_file = "order.txt";
+string user_file = "user.txt";
+string path = "/Users/huwenjing/project01/";
+
+
+struct commodity{ //Todo:结构
+
+};
+
+struct every_user{
+
+};
+
+struct order{
+
+};
+
 #endif //PROJECT01__DATA_H_
--- sql.h
+++ sql.h
@@ -5,4 +5,16 @@
 #ifndef PROJECT01__SQL_H_
 #define PROJECT01__SQL_H_
 
+#include <iostream>
+using namespace std;
+void mysql(string instruction);
+void insert(string table_name);
+void insert_user(string x1, string x2, string x3, string x4, string x5, string x6);
+void insert_good(string x1, string x2, string x3, string x4, string x5, string x6, string x7, string x8);
+void insert_order(string x1, string x2, string x3, string x4, string x5, string x6, string x7);
+void delete_(string table_name, string column, string value);
+void update(string table_name, string column, string value, string edit_column, string edit_value);
+void select_admin(string table, string column, string value);
+void select_buyer(string table, string column, string value);
+void select_all(string user, string table);//buyer,seller,admin
 #endif //PROJECT01__SQL_H_
