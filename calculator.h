1ab872302524d1a6c51124cfe19f92ca807bc9c1
Leticia-huwenjing <1502503185@qq.com>
2022-02-19 15:07:19 +0800

    The corresponding files are initialized 2.0

---
+++ calculator.h
@@ -0,0 +1,8 @@
+//
+// Created by 胡文菁 on 2022/2/19.
+//
+
+#ifndef PROJECT01__CALCULATOR_H_
+#define PROJECT01__CALCULATOR_H_
+
+#endif //PROJECT01__CALCULATOR_H_
--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include "admin.h"
+#include "buyer.h"
+#include "calculator.h"
+#include "data.h"
+#include "seller.h"
+#include "sql.h"
+#include "user.h"
+using namespace std;
 
 int main() {
 
