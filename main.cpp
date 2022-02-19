e47b44dd3ce528d94bb32804a2000669c90de962
Leticia-huwenjing <1502503185@qq.com>
2022-02-19 19:52:33 +0800

    The corresponding files are initialized 3.0

--- main.cpp
+++ main.cpp
@@ -8,7 +8,60 @@
 #include "user.h"
 using namespace std;
 
-int main() {
+char opt;
 
-  return 0;
-} 
+void main_manu(){
+  cout << "==========================================" << endl;
+  cout << "1.管理员登录 2.用户注册 3.用户登录 4.退出程序" << endl;
+  cout << "==========================================" << endl << endl;
+  cout << "请输入操作：";
+}
+
+void admin_mod(){
+  ;
+}
+
+void register_account(){
+  ;
+}
+
+void user_module(){
+  ;
+}
+
+int main()
+{
+  while (true)
+  {
+    main_manu();
+    cin >> opt;
+    switch (opt)
+    {
+      case '1':{
+        char c;
+        while(c=getchar() != '\n');
+        admin_mod();
+        break;
+      }
+      case '2':{
+        char c;
+        while(c=getchar() != '\n');
+        register_account();
+        break;
+      }
+      case '3':{
+        char c;
+        while(c=getchar() != '\n');
+        user_module();
+        break;
+      }
+      case '4':return 0;
+      default: {
+        char c;
+        while(c=getchar() != '\n');
+        cout << "Not A Valid Request!" << endl;
+        break;
+      }
+    }
+  }
+}
