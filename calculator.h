766c83d094d1d601afdf2fbf19f32eec5c5302cb
Leticia-huwenjing <1502503185@qq.com>
2022-02-19 23:09:38 +0800

    The corresponding files are initialized 3.2

--- calculator.h
+++ calculator.h
@@ -5,4 +5,10 @@
 #ifndef PROJECT01__CALCULATOR_H_
 #define PROJECT01__CALCULATOR_H_
 
+#include <iostream>
+#include "user.h"
+using namespace std;
+string calculator(string edited_notation); //返回表达式的值或表达式不合法的原因
+string edit(string notation); //将中缀表达式转化为逆波兰表达式
+string generate(User &user); //产生中缀表达式
 #endif //PROJECT01__CALCULATOR_H_
