852f7c0d87f36bfcedac06835ddb97c444ea0ecf
Leticia-huwenjing <1502503185@qq.com>
2022-02-20 19:03:28 +0800

    The corresponding files are initialized 3.4

--- data.h
+++ data.h
@@ -11,7 +11,7 @@ using namespace std;
 string user_title = "用户ID,用户名,密码,联系方式,地址,钱包余额";
 string good_title = "商品ID,名称,价格,数量,描述,卖家ID,上架时间,商品状态";
 string order_title = "订单ID,商品ID,交易单价,数量,交易时间,卖家ID,买家ID";
-string command_file = "command.txt";
+string command_file = "commands.txt";
 string good_file = "commodity.txt";
 string order_file = "order.txt";
 string user_file = "user.txt";
@@ -19,11 +19,23 @@ string path = "/Users/huwenjing/project01/";
 
 
 struct commodity{ //Todo:结构
-
+  string condition;
+  string good_id;
+  string good_name;
+  double good_price=0;
+  int stock=0;
+  string information;
+  string time;
+  string seller_id;
 };
 
 struct every_user{
-
+  string user_id;
+  string user_name;
+  string password;
+  string tel;
+  string address;
+  double money;
 };
 
 struct order{
