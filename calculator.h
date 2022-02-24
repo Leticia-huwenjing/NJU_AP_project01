fa7acecf55494c229ca011740995ed4528a55fac
Leticia-huwenjing <1502503185@qq.com>
2022-02-24 16:43:17 +0800

    user version 1.1，admin version 1.0，calculator 1.0, 完成了用户登录&用户注册，完成了管理员登录,实现了计算器功能及报错

--- calculator.h
+++ calculator.h
@@ -6,9 +6,29 @@
 #define PROJECT01__CALCULATOR_H_
 
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <cmath>
 #include "user.h"
 using namespace std;
-string calculator(string edited_notation); //返回表达式的值或表达式不合法的原因
-string edit(string notation); //将中缀表达式转化为逆波兰表达式
-string generate(User &user); //产生中缀表达式
+
+bool point_valid(string notation);//接受原始表达式，判断如果有小数点，小数点左右是否有空格
+bool blank_valid(string notation);//接受原始表达式，判断空格是否合法，一个空格的前后不能都是数字，负号与数字之间不应该有空格，比如，1 2 3即为不合法，-  7不合法
+string edit(string notation);//去除表达式中所有的空格
+bool add_valid(string notation);//保证加号是双元操作符
+bool sub_valid(string notation);//保证减号是双元操作符
+bool multi_valid(string notation);//保证乘号是双元操作符
+bool div_valid(string notation);//保证除号是双元操作符
+bool mul_valid(string notation);//保证无隐藏乘号
+bool operator_valid(string notation);//保证无连续操作符
+bool bracket_valid(string notation);//保证括号匹配且不存在空括号
+bool decimal_valid(string notation);//保证不出现0. .0
+bool digit_valid(string notation);//保证只有一位小数
+string is_valid(string notation);//此时输入的表达式不含有空格,如果不合法，返回不合法原因，如果合法，返回"合法"
+int priority(string str);
+string convert(string notation);//将中缀表达式转为逆波兰表达式,在逆波兰表达式中，将&当作负号
+string calculate(string notation);//计算逆波兰表达式,返回除零错误
+string calculator(string notation);//总计算器接口
+
 #endif //PROJECT01__CALCULATOR_H_
